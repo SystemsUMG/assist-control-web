e4338a05ee28c6249ca24bcf5755e56050c9cb81
Cristian González <crisgon1065@gmail.com>
2022-10-21 16:45:36 -0600

    Implementacion de login/logout con modelos teacher y student

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,47 +3,46 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Response\ResponseController;
 use App\Http\Requests\AuthLoginRequest;
+use App\Models\Student;
+use App\Models\Teacher;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
-class AuthController extends Controller
+class AuthController extends ResponseController
 {
-    public $response;
-    public $message;
-    public $result;
-    public $records;
-    public $statusCode;
-
-    public function __construct()
-    {
-        $this->response = New ResponseController();
-        $this->message = 'Bienvenido(a)';
-        $this->result = true;
-        $this->records = [];
-        $this->statusCode = 200;
-    }
 
     public function login(AuthLoginRequest $request)
     {
         $credentials = $request->validated();
-        if (!auth()->attempt($credentials)) {
+        //Autenticar con ambos modelos
+        if (!auth()->attempt($credentials) && !auth('api')->attempt($credentials)) {
             $this->message = 'Credenciales incorrectas';
             $this->statusCode = 401;
             $this->result = false;
         } else {
-            $user = User::where('email', $request->email)->firstOrFail();
+            $student = Student::firstWhere('email', $request->email);
+            $teacher = Teacher::firstWhere('email', $request->email);
+            //Validar student o teacher
+            $user = $student ?? $teacher;
+            $type = $student ? 'student' : 'teacher';
             $token = $user->createToken('auth-token');
 
+            $this->statusCode = 200;
+            $this->result = true;
+            $this->message = 'Bienvenido(a)';
             $this->records = [
                 'user_id' => $user->id,
-                'name' => $user->name,
-                'token' => $token->plainTextToken,
+                'name'    => $user->name,
+                'type'    => $type,
+                'token'   => $token->plainTextToken,
             ];
         }
 
-        return $this->response->jsonResponse($this->records, $this->result, $this->message, $this->statusCode);
+        return $this->jsonResponse($this->records, $this->result, $this->message, $this->statusCode);
     }
 
     /**
@@ -51,9 +50,10 @@ class AuthController extends Controller
      */
     public function logout()
     {
-        Auth::user()->tokens()->delete();
-
+        Auth::user()->currentAccessToken()->delete();
+        $this->statusCode = 200;
+        $this->result = true;
         $this->message = 'Se ha cerrado la sesión';
-        return $this->response->jsonResponse($this->records, $this->result, $this->message, $this->statusCode);
+        return $this->jsonResponse($this->records, $this->result, $this->message, $this->statusCode);
     }
 }
