1ec87237dd634b2c34cef8ab780462667d6e6284
Cristian González <crisgon1065@gmail.com>
2022-10-21 23:20:13 -0600

    Fronten crud para centros

--- resources/js/pages/partials/Sidenav.vue
+++ resources/js/pages/partials/Sidenav.vue
@@ -3,8 +3,8 @@
     <div class="sidenav-header">
       	<i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       	<router-link class="navbar-brand m-0" :to="{ name: 'Home' }">
-      	  	<img :src="'/img/logoRH.png'" class="navbar-brand-img h-100" alt="main_logo">
-      	  	<span class="ms-1 font-weight-bold">Control de Personal</span>
+			<img :src="'/img/logoRH.png'" class="navbar-brand-img h-100" alt="main_logo">
+			<span class="ms-1 font-weight-bold">Control de Personal</span>
       	</router-link>
     </div>
     <hr class="horizontal dark mt-0">
@@ -24,6 +24,14 @@
         	  	  	  	<i class="ni ni-single-02 text-warning text-sm opacity-10"></i>
         	  	  	</div>
         	  	  	<span class="nav-link-text ms-1">Usuarios</span>
+        	  	</router-link>
+        	</li>
+        	<li class="nav-item">
+        	  	<router-link class="nav-link " :to="{ name: 'Centers' }">
+					<div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+        	  	  	  	<i class="ni ni-square-pin text-warning text-sm opacity-10"></i>
+					</div>
+					<span class="nav-link-text ms-1">Centros</span>
         	  	</router-link>
         	</li>
         	<li class="nav-item">
--- resources/js/router.js
+++ resources/js/router.js
@@ -7,6 +7,7 @@ import Countries from "./pages/countries/Index.vue";
 import Departments from "./pages/departments/Index.vue";
 import Regions from "./pages/regions/Index.vue";
 import Roles from "./pages/roles/Index.vue";
+import Centers from "./pages/centers/Index.vue";
 import NotFound from "./pages/NotFound.vue";
 
 const routes = [
@@ -49,6 +50,11 @@ const routes = [
         path: "/roles",
         name: "Roles",
         component: Roles
+    },
+    {
+        path: "/centers",
+        name: "Centers",
+        component: Centers
     },
     {
         path: "/:catchAll(.*)",
